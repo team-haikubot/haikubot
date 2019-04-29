d32bf8ed86035b84ccd9fa6c707a976e656c52d6
Ben Parisot <me@benparisot.com>
2019-04-29 16:24:53 -0700

    syllable counting function

--- test/middleware/syllable-count.test.js
+++ test/middleware/syllable-count.test.js
@@ -1,16 +1,17 @@
-const { countFive, countSeven } = require('../../lib/middleware/syllable-count');
+const { countSyllable } = require('../../lib/utils/countSyllable');
 
 
 describe('Syllable Count Tests', () => {
-  it('will take a string of text and count 5 syllables', done => {
+  it('will take a string of text and count 5 syllables', () => {
     const text = 'like the morning dew';
-
-    const req = { text };
-    const res = {};
-    const next = () => {
-      expect(req.body).toEqual(5);
-      done();
-    };
-    countFive(req, res, next);
+    const result = countSyllable(text);
+    expect(result).toEqual(5);
+    // const req = { text };
+    // const res = {};
+    // const next = () => {
+    //   expect(req.body).toEqual(5);
+    //   done();
+    // };
+    // countFive(req, res, next);
   });
 });
