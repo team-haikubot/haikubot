e765c3bf67dfcb50cf37f6e2af549941c01e26e8
Ben Parisot <me@benparisot.com>
2019-04-29 14:01:56 -0700

    signin test passed+route written

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -27,5 +27,34 @@ describe('Auth Routes Tests', () => {
             
         });
       });
-  });   
+  }); 
+  
+  it('signs in a user', () => {
+    return User.create({
+      email: 'testy@getoffmyback.com',
+      username: 'spicy',
+      password: 'ham',
+      twitterHandle: 'yup'
+    })
+      .then(() => {
+        return request(app)
+          .post('/api/v1/auth/signin')
+          .send({
+            username: 'spicy',
+            password: 'ham',
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          user: {
+            __v: 0,
+            _id: expect.any(String),
+            email: 'testy@getoffmyback.com',
+            username: 'spicy',
+            twitterHandle: 'yup'
+          }, token: expect.any(String)
+        });
+      });
+
+  });
 });
