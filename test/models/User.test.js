2aa86790d593e51bc091a18fa1868d7e21d2447e
Ben Parisot <me@benparisot.com>
2019-05-02 15:59:04 -0700

    pass User model test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -5,15 +5,11 @@ const User = require('../../lib/models/User');
 describe('User Model test', () => {
   it('can create a new user', () => {
     const newUser = new User({
-      email: 'test@email.net',
       username: 'Banana',
-      password: 'testpw',
-      twitterHandle: 'fakeTwitter'
+      password: 'testpw'
     });
     expect(newUser.toJSON()).toEqual({
-      email: 'test@email.net',
       username: 'Banana',
-      twitterHandle: 'fakeTwitter',
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
